812d3ae27bd7f3d0cdcfe749722afc3027ab8cb4
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-08-16 16:25:57 +0300

    initial commit

---
+++ pages/getting-started.mdx
@@ -0,0 +1,70 @@
+## Quick start
+
+This is a Next.js project bootstrapped with create-next-app. You'll need to install Node.js (v16 or up) before using
+Dashboard.
+
+Before proceeding, please ensure you have the following software installed on your computer.
+
+- Node
+- Yarn (optional but recommended)
+- Git command line tools
+
+## Useful links
+- Download Git CLI -
+    - Windows: https://git-scm.com/download/windows
+    - Mac: https://git-scm.com/download/mac
+- Download Node - https://nodejs.org/en/
+- Download Yarn CLI - https://yarnpkg.com/lang/en/docs/install/
+- Download IDE
+    - vsCode: https://code.visualstudio.com/
+    - Jetbrains webstorm - https://www.jetbrains.com/webstorm/
+
+## Project Setup
+In your root project folder, open your terminal and run the command below.
+
+```bash copy
+npm install
+```
+
+Now you're ready to modify the source files and generate new files. To automatically detect file changes and start a
+local webserver at http://localhost:3000, run the following command.
+
+```bash copy
+npm run dev
+```
+
+## Build tools
+Start a local webserver at http://localhost:3000 and detect file changes:
+
+```bash copy
+npm run dev
+```
+
+Compile, optimize, minify and uglify all source files to build/:
+
+```bash copy
+npm run build
+```
+
+## File structure
+Inside the zip-file you'll find the following directories and files. Both compiled and minified distrubution files, as
+well as the source files are included in the package.
+
+```
+theme/
+  ├── .gitignore
+  ├── package.json
+  ├── package-lock.json
+  ├── README.md
+  ├── components/
+  ├── layout/
+  ├── mocks/
+  ├── pages/
+  ├── routes/
+  ├── styles/
+  ├── types/
+  ├── utils/
+  ├── public/
+  │   ├── index.html
+  │   └── manifest.json
+```
---
+++ theme.config.jsx
@@ -0,0 +1,58 @@
+import {useRouter} from "next/router";
+import {useConfig} from 'nextra-theme-docs'
+import Favicon32 from "./static/favicon-32x32.png";
+import Favicon16 from "./static/favicon-16x16.png";
+
+export default {
+    useNextSeoProps() {
+        const {asPath} = useRouter()
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s | DesignSparx'
+            }
+        }
+    },
+    head: () => {
+        const {asPath, defaultLocale, locale} = useRouter()
+        const {frontMatter} = useConfig()
+        const url =
+            'https://my-app.com' +
+            (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+        return (
+            <>
+                <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png"/>
+                <link rel="icon" type="image/x-icon" href="/static/favicon.ico"/>
+                <link rel="manifest" href="static/site.webmanifest"/>
+                <meta property="og:url" content={url}/>
+                <meta property="og:title" content={frontMatter.title || 'Design Sparx Docs'}/>
+                <meta
+                    property="og:description"
+                    content={frontMatter.description || 'Explore our versatile dashboard website template featuring a stunning array of themes and meticulously crafted components. Elevate your web project with seamless integration, customizable themes, and a rich variety of components for a dynamic user experience. Effortlessly bring your data to life with our intuitive dashboard template, designed to streamline development and captivate users. Discover endless possibilities in design and functionality today!'}
+                />
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+                <meta
+                    name="description"
+                    content="Explore our versatile dashboard website template featuring a stunning array of themes and meticulously crafted components. Elevate your web project with seamless integration, customizable themes, and a rich variety of components for a dynamic user experience. Effortlessly bring your data to life with our intuitive dashboard template, designed to streamline development and captivate users. Discover endless possibilities in design and functionality today!"/>
+            </>
+        )
+    },
+    project: {
+        link: 'https://github.com/shuding/nextra'
+    },
+    logo: (
+        <>
+            <img src="static/logo.png" alt="Design Sparx Docs" height={16} width={16} style={{objectFit: 'contain'}}/>
+            <span style={{marginLeft: '.4em', fontWeight: 800}}>
+                Design Sparx
+            </span>
+        </>
+    ),
+    sidebar: {
+        toggleButton: true,
+        autoCollapse: true
+    },
+    editLink: false
+}
